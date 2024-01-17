534aad52f73514a3c52718ea53f15c25d17fbc7a
Axocoder <126685802+Axocoder0@users.noreply.github.com>
2024-01-17 22:51:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from flask import Flask, render_template
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''<body style="margin: 0; padding: 0;">
+    <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+  </body>'''
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():  
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+print("Server Running Because of Axo")
+
 import requests
 import json
 import time
